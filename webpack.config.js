8ed57a8e93dcc8aff173b610b0137500612ce80a
Rage Gage <gnewman@appacademy.io>
2017-04-26 13:08:49 -0700

    initial config done

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  entry: './entry.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
