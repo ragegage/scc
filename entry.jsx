58062d7200c0cdc7b9cf2cca6df9a41a2c34da9b
Rage Gage <gnewman@appacademy.io>
2017-04-27 13:18:25 -0700

    rough out basics of dept selection buttons

--- entry.jsx
+++ entry.jsx
@@ -4,14 +4,35 @@ import data from './data/'
 import Salaries from './features/salary/'
 import Employees from './features/employees/'
 
-const App = () => (
-  <div className="react-app">
-    react working
-    <Salaries data={data} />
-    <Employees data={data} />
-    filter for ^ (pick one department)
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {selected: undefined}
+    this.depts = [...new Set(data.map(em => em.dept))]
+  }
+  
+  changeDept(name) {
+    return (e) => {
+      e.preventDefault()
+      this.setState({selected: name})
+    }
+  }
+  
+  render() {
+    console.log(this.depts)
+    console.log(this.state.selected)
+    return (
+      <div className="react-app">
+        react working
+        <Salaries data={data} />
+        <Employees data={data} />
+        {this.depts.map(dept => (
+          <button onClick={this.changeDept(dept)}>{dept}</button>
+        ))}
+      </div>
+    )
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   render(<App />, document.getElementById("app"))
