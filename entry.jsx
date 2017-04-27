610c49af699128104e7f6e860f88b5bf41b1c0e5
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-26 19:29:46 -0700

    rough in salary function

--- entry.jsx
+++ entry.jsx
@@ -1,9 +1,28 @@
 import { render } from 'react-dom'
 import React from 'react'
+import data from './data/'
+
+const Salary = (d) => {
+  const salaries = d.map(em => em.salary)
+  const salaryCount = salaries.length
+  const totalSalary = salaries.reduce((a, s) => a + s)
+  return totalSalary / salaryCount
+}
+
+const employeeChart = (d) => {
+  
+}
 
 const App = () => (
   <div className="react-app">
     react working
+    <div className="salary">
+      avg salary: ${Salary(data)}
+    </div>
+    <div className="chart">
+      # employees vs. time
+    </div>
+    filter for ^ (pick one department)
   </div>
 )
 
