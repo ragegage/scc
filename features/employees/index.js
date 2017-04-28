6fa67226b362728e19b075e90b34c47abb74795e
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-27 22:40:16 -0700

    add axis labels to employees chart

--- features/employees/index.js
+++ features/employees/index.js
@@ -5,7 +5,12 @@ import COLORS from '../../assets/colors.js'
 const employeeChart = (d) => {
   return d.map(em => em.date)
           .sort()
-          .map((date, count) => ({x: toYear(date), y: count + 1}))
+          .map((date, count) => ({
+            date: toYear(date),
+            employeeCount: count + 1
+          }))
+          .concat({date: 2018, employeeCount: d.length})
+        // ^ hack to make them all end at the same point in time (2018)
 }
 
 const toYear = (date) => (
@@ -14,12 +19,19 @@ const toYear = (date) => (
 
 export default ({ data }) => (
   <div className="employees">
-    <h1># Employees over time</h1>
     <V.VictoryChart>
+      <V.VictoryAxis
+        dependentAxis
+      />
+      <V.VictoryAxis
+        tickValues={[2015, 2016, 2017, 2018]}
+      />
       <V.VictoryArea
         interpolation="stepAfter"
         data={employeeChart(data.employees)}
         style={{data: {fill: `${COLORS[data.dept]}`}}}
+        x="date"
+        y="employeeCount"
         />
     </V.VictoryChart>
   </div>
