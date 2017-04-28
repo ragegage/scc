24cc531072636ffed6b4c6ebec2100911b93e4de
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-28 08:00:08 -0700

    start refactoring Salary component

--- features/salary/index.js
+++ features/salary/index.js
@@ -2,44 +2,44 @@ import React from 'react'
 import * as V from 'victory'
 import COLORS from '../../assets/colors.js'
 
-const Salary = (d) => {
-  const salaries = d.map(em => em.salary)
+const avgSalary = (employeeList) => {
+  const salaries = employeeList.map(em => em.salary)
   const salaryCount = salaries.length
   const totalSalary = salaries.reduce((a, s) => a + s)
   return Math.round(totalSalary / salaryCount * 100) / 100
 }
 
-const Salaries = ({ data }) => {
-  const avgSalaries = Object.values(data).map(datum => (
-    {dept: datum.dept, salary: Salary(datum.employees)}
+const avgSalaries = (employeeList) => (
+  Object.values(employeeList).map(datum => (
+    {dept: datum.dept, salary: avgSalary(datum.employees)}
   ))
+)
 
-  return (
-    <div className="salary">
-      <V.VictoryChart domainPadding={40}>
-        <V.VictoryAxis
-          dependentAxis
-          tickFormat={(salary) => (`$${salary / 1000}k`)}
-        />
-        <V.VictoryAxis />
-        <V.VictoryBar
-          labels={(d) => `$${d.y}`}
-          labelComponent={<V.VictoryTooltip
-                            dy={-10}
-                            pointerLength={0}
-                            style={{fontFamily: 'Source Sans Pro',
-                          backgroundColor: '#eee'}}
-                          />}
-          style={{
-            data: {fill: (d) => COLORS[d.x]}
-          }}
-          data={avgSalaries}
-          x="dept"
-          y="salary"
-        />
-      </V.VictoryChart>
-    </div>
-  )
-}
+const Salaries = ({ data }) => (
+  <div className="salary">
+    <V.VictoryChart domainPadding={40}>
+      <V.VictoryAxis
+        dependentAxis
+        tickFormat={(salary) => (`$${salary / 1000}k`)}
+      />
+      <V.VictoryAxis />
+      <V.VictoryBar
+        labels={(d) => `$${d.y}`}
+        labelComponent={<V.VictoryTooltip
+                          dy={-10}
+                          pointerLength={0}
+                          style={{fontFamily: 'Source Sans Pro',
+                        backgroundColor: '#eee'}}
+                        />}
+        style={{
+          data: {fill: (d) => COLORS[d.x]}
+        }}
+        data={avgSalaries(data)}
+        x="dept"
+        y="salary"
+      />
+    </V.VictoryChart>
+  </div>
+)
 
 export default Salaries
