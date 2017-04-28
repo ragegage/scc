6f8864c582c2f7718af7744d18ed3217146f5c07
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-27 23:00:48 -0700

    style buttons and tooltips

--- features/salary/index.js
+++ features/salary/index.js
@@ -24,7 +24,12 @@ const Salaries = ({ data }) => {
         <V.VictoryAxis />
         <V.VictoryBar
           labels={(d) => `$${d.y}`}
-          labelComponent={<V.VictoryTooltip dy={-10} pointerLength={0} />}
+          labelComponent={<V.VictoryTooltip
+                            dy={-10}
+                            pointerLength={0}
+                            style={{fontFamily: 'Source Sans Pro',
+                          backgroundColor: '#eee'}}
+                          />}
           style={{
             data: {fill: (d) => COLORS[d.x]}
           }}
