5d1dfc1b9f2b408e3361d576b9cb87bec817f567
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-28 19:38:40 -0700

    add one basic integration test

--- tests/entry.test.js
+++ tests/entry.test.js
@@ -1,3 +1,14 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+
+import App from '../app'
+
 test('asserts basic logic of universe', () => {
-  expect((1 + 2)).toBe(3);
-});
+  expect((1 + 2)).toBe(3)
+})
+
+describe('<App />', () => {
+  it('should render and be selectable by class "react-app"', function() {
+    expect(shallow(<App />).is('.react-app')).toBe(true);
+  });
+})
